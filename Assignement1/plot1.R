aeca355070436a0297735093faea98df1f445bd4
André Amorim <amorim.andre.89@gmail.com>
2014-06-09 00:17:55 +0100

    version 3

--- Assignement1/plot1.R
+++ Assignement1/plot1.R
@@ -7,7 +7,7 @@ usefulData$Time = as.character(usefulData$Time)
 DateTime = paste(usefulData$Date, usefulData$Time)
 usefulData$DateTime = strptime(DateTime, format="%d/%m/%Y %H:%M:%S")
 any(usefulData=="?")
-png(file="plot1.png",bg =NA,width = 505, height = 505, units = "px",type =  "cairo")
+png(file="plot1.png",bg =NA,width = 480, height = 480, units = "px",type =  "cairo")
 with(usefulData,hist(Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power"))
 dev.off()
 
